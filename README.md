c192f10982d02c935eff2cf058e6b3a12957be47
mingyi.ymy <mingyi.ymy@alibaba-inc.com>
2017-08-28 15:05:58 +0800

    fet: 新增配置选项

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 fis3-prepackager-site-hash
 =====================
 
-fis3 文件中获取站点所有文件内容总和的hash值
+[![npm](https://img.shields.io/npm/v/fis3-prepackager-site-hash.svg)](https://www.npmjs.com/package/fis3-prepackager-site-hash)
+[![npm](https://img.shields.io/npm/dm/fis3-prepackager-site-hash.svg)](https://www.npmjs.com/package/fis3-prepackager-site-hash)
+
+fis3 文件中获取站点所有静态文件内容总和的md5序列
 
 ## 安装
 
@@ -11,18 +14,18 @@ fis3 文件中获取站点所有文件内容总和的hash值
 
 ## 使用方法
 
-### 配置
+### 基础配置
 
 ```js
 fis.match('::package', {
-  prepackager: fis.plugin('site-hash')
+  prepackager: fis.plugin('site-hash'[, options])  // { length: 7 } 序列长度（默认为7）
 })
 ```
 
 ### 文件中使用
 
 ```js
-var thisFileHash = '__siteHash__';
+var siteHash = '__siteHash__';
 ```
 
 
--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 var reg = /('|")__siteHash__\1/mg;
 
 module.exports = function (ret, pack, settings, opt) {
+
   fis.util.map(ret.src, function (subpath, file) {
     var content = file.getContent();
     if (content.replace) {
       content = content.replace(reg, function (str, quote) {
-        var hash = getSiteHash(ret);
+        var hash = getSiteHash(ret, settings);
         return quote + hash + quote;
       })
       file.setContent(content);
@@ -14,10 +15,14 @@ module.exports = function (ret, pack, settings, opt) {
   });
 };
 
-function getSiteHash(ret) {
+function getSiteHash(ret, settings) {
   var str = '';
+  var len = 7;
+  if (settings && settings.length && typeof settings.length === 'number') {
+    len = settings.length;
+  }
   fis.util.map(ret.src, function(subpath, file){
       str += file.getContent();
   });
-  return fis.util.md5(str) || '';
+  return fis.util.md5(str, len) || '';
 }
